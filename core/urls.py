fb78e51083368658e795b3328af199d47766b164
Gedamu Ayana <your_email@example.com>
2026-06-14 00:47:11 +0300

    Clean up URL configurations

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from django.urls import path
 from .views import signup_view
+
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('deposit/', views.submit_deposit, name='deposit'),
@@ -11,6 +11,3 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('signup/', signup_view, name='signup'),
 ]
-from django.urls import path
-from .views import signup_view  # ይህንን ከላይ መጨመር እንዳትረሳ
-
--- my_shop/urls.py
+++ my_shop/urls.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import signup_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-path('signup/', signup_view, name='signup'),
-    path('', include('core.urls')),
+    path('', include('core.urls')), # የ core ሊንኮችን (signupን ጨምሮ) እዚህ ጋር ያገናኛል
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
