0f6da34770430799e0cdf6ce887f152fe5b5e44a
Gedamu Ayana <your_email@example.com>
2026-06-14 00:42:25 +0300

    Fix signup URL routing in main project

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from core.views import signup_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+path('signup/', signup_view, name='signup'),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
