f6012e8f81572ca247049efdc4378a2bdb212ae8
Gedamu Ayana <your_email@example.com>
2026-06-14 15:49:48 +0300

    Fix my_shop urls routing

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from core import views # ከ . ፋንታ core ብለው ያርሙት
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'),
-    path('register/', views.register_view, name='register'),
-    # የቀሩትን መስመሮች እንዳለ ይተዉዋቸው...
-]
+    path('', include('core.urls')), # በቀጥታ ወደ core urls.py ይወስደዋል
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
