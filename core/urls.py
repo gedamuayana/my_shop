dc025185ace4d7d17f2b9d129f8e4fee645dd590
Gedamu Ayana <your_email@example.com>
2026-06-14 15:55:26 +0300

    Clean up urls and routing

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
+
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.home_view, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('deposit/', views.submit_deposit, name='deposit'),
     path('complete-order/', views.complete_order, name='complete_order'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
--- my_shop/urls.py
+++ my_shop/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')), # በቀጥታ ወደ core urls.py ይወስደዋል
+    path('', include('core.urls')), # በቀጥታ ወደ core/urls.py ይመራል
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
