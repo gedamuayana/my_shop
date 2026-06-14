3e972951b8feb20e8797ccef0e5664098c3c0fd0
Gedamu Ayana <your_email@example.com>
2026-06-14 17:08:50 +0300

    Fix i18n redirect root url to en

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -3,15 +3,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
-from django.views.generic.base import RedirectView
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
-    # ባዶ ዩአርኤል ሲገባ በቀጥታ ወደ ትክክለኛው የ en-us ቋንቋ ራውተር ይመራል
-    path('', RedirectView.as_view(url='/en-us/', permanent=False)),
+    # ባዶ ዩአርኤል ሲገባ በቀጥታ ወደ እንግሊዝኛ ቋንቋ ራውተር (en) ይመራል
+    path('', lambda request: redirect('/en/')),
 ]
 
 urlpatterns += i18n_patterns(
-    path('', include('core.urls')), # በቀጥታ ወደ core/urls.py ይመራል
+    path('', include('core.urls')), # የቋንቋ ምርጫ የሚደረግባቸው ገጾች እዚህ ይካተታሉ
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
