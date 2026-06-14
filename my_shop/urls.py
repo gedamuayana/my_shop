15d607b881bde1f025995432771889f4bd68498d
Gedamu Ayana <your_email@example.com>
2026-06-14 21:09:42 +0300

    Fix routing and completely restore homepage

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -2,26 +2,26 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib.auth import views as auth_views
+from core import views  # ሆም ፔጅ ቪው እንዲነበብ
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n')),
-]
+                  path('admin/', admin.site.urls),
 
-urlpatterns += i18n_patterns(
-    # ዋናዎቹ የድረ-ገጽ ሊንኮች (login, register, home, dashboard...)
-    path('', include('core.urls')),
+                  # ዋናው መግቢያ ገጽ (Home page) በቀጥታ እንዲነበብ
+                  path('', views.home_view, name='home'),
+                  path('', include('core.urls')),
 
-    # የይለፍ ቃል መቀየሪያ ሊንኮች (Forgot Password)
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'),
-         name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
-         name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
-         name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
-         name='password_reset_complete'),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  # የይለፍ ቃል መቀየሪያ ሊንኮች (Forgot Password)
+                  path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'),
+                       name='password_reset'),
+                  path('password_reset/done/',
+                       auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+                       name='password_reset_done'),
+                  path('reset/<uidb64>/<token>/',
+                       auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+                       name='password_reset_confirm'),
+                  path('reset/done/',
+                       auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+                       name='password_reset_complete'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
