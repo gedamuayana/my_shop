ce08bbb3e2487370c712e05e1df18faa50e33dcf
Gedamu Ayana <your_email@example.com>
2026-06-14 15:47:44 +0300

    Fix views import in my_shop urls

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from core import views # ከ . ፋንታ core ብለው ያርሙት
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.home_view, name='home'),
+    path('register/', views.register_view, name='register'),
+    # የቀሩትን መስመሮች እንዳለ ይተዉዋቸው...
+]
