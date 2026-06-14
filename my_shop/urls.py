e0cf4b60994635dfd42f189fa77a23fd16d271ab
Gedamu Ayana <your_email@example.com>
2026-06-14 17:21:39 +0300

    Add LocaleMiddleware and fix i18n routing

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # ቀጥታ ወደ ዋናው ሲስተም ይወስዳል
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('i18n/', include('django.conf.urls.i18n')),
+    # ባዶ ዩአርኤል ሲገባ በቀጥታ ወደ እንግሊዝኛ ቋንቋ ይመራል
+    path('', lambda request: redirect('/en/')),
+]
+
+urlpatterns += i18n_patterns(
+    path('', include('core.urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
